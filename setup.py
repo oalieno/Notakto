617faaadd22a8122545a96251e2fd4e5992eea2e
OAlienO <jeffrey6910@gmail.com>
2018-01-02 00:56:51 +0800

    v1.1

--- setup.py
+++ setup.py
@@ -4,11 +4,11 @@ from distutils.core import setup
 setup(
     name = 'Notakto',
     packages = ['Notakto'],
-    version = '1.0',
+    version = '1.1',
     description = 'Notakto AI',
     author = 'OAlienO',
     author_email = 'jeffrey6910@gmail.com',
     url = 'https://github.com/OAlienO/Notakto',
-    download_url = 'https://github.com/OAlienO/Notakto/archive/1.0.tar.gz',
+    download_url = 'https://github.com/OAlienO/Notakto/archive/1.1.tar.gz',
     keywords = ['notakto', 'math', 'game']
 )
