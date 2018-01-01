c445fbf8409eca826cfccf8c4433cd486334dba0
OAlienO <jeffrey6910@gmail.com>
2018-01-02 00:23:36 +0800

    setup and upload to pypi

---
+++ setup.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+from distutils.core import setup
+
+setup(
+    name = 'Notakto',
+    packages = ['Notakto'],
+    version = '1.0',
+    description = 'Notakto AI',
+    author = 'OAlienO',
+    author_email = 'jeffrey6910@gmail.com',
+    url = 'https://github.com/OAlienO/Notakto',
+    download_url = 'https://github.com/OAlienO/Notakto/archive/1.0.tar.gz',
+    keywords = ['notakto', 'math', 'game']
+)
